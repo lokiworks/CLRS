193d86d0fe5c8fc0274b45ed27ebe7e4f5dbfe41
Loki <lokiworks.yen@gmail.com>
2022-07-09 14:33:39 +0800

    feat: add v2 answer for the question three.

--- yan/leetcode/src/q3_test.go
+++ yan/leetcode/src/q3_test.go
@@ -46,3 +46,48 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		})
 	}
 }
+
+func Test_lengthOfLongestSubstringV2(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			"withLength4",
+			args{s: "abcaddcbb"},
+			4,
+		},
+		{
+			"withOneChar",
+			args{s: "c"},
+			1,
+		},
+		{
+			"withEmptyString",
+			args{s: ""},
+			0,
+		},
+		{
+			"withSpaceString",
+			args{s: " "},
+			1,
+		},
+		{
+			"withTwoChar",
+			args{s: "cu"},
+			2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := lengthOfLongestSubstringV2(tt.args.s); got != tt.want {
+				t.Errorf("lengthOfLongestSubstringV2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
