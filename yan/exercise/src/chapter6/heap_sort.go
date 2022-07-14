cf0cb928d355c788218557366d931a458d98982b
peter <loki.yen@outlook.com>
2022-07-14 09:53:23 +0800

    feat: add implement of the heap sort.

---
+++ yan/exercise/src/chapter6/heap_sort.go
@@ -0,0 +1,55 @@
+package main
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+func left(i int) int {
+	return 2*(i+1) - 1
+}
+
+func right(i int) int {
+	return 2 * (i + 1)
+}
+
+func maxHeapify(a []int, heapSize, i int) {
+	l := left(i)
+	r := right(i)
+	largestIndex := -1
+	if l < heapSize {
+		if a[l] > a[i] {
+			largestIndex = l
+		} else {
+			largestIndex = i
+		}
+	}
+	if r < heapSize {
+		if a[r] > a[largestIndex] {
+			largestIndex = r
+		}
+	}
+	if largestIndex != -1 && largestIndex != i {
+		// exchange a[i] with a[]
+		a[i], a[largestIndex] = a[largestIndex], a[i]
+		maxHeapify(a, heapSize, largestIndex)
+	}
+
+}
+
+func buildMaxHeap(a []int) {
+	l := len(a) - 1
+	for i := l / 2; i >= 0; i-- {
+		maxHeapify(a, l, i)
+	}
+}
+
+func headSort(a []int) []int {
+	buildMaxHeap(a)
+	l := len(a)
+	for i := len(a) - 1; i >= 1; i-- {
+		a[0], a[i] = a[i], a[0]
+		l--
+		maxHeapify(a, l, 0)
+
+	}
+	return a
+}
