9ba9185b9c6d0f965c7c999df2ac3977dafba0b7
Loki <lokiworks.yen@gmail.com>
2022-07-17 19:43:17 +0800

    feat: add answer for the question twenty-eight.

---
+++ yan/leetcode/src/q28_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func Test_strStr(t *testing.T) {
+	type args struct {
+		haystack string
+		needle   string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "WithFoundSubString",
+			args: args{haystack: "hello", needle: "ll"},
+			want: 2,
+		},
+		{
+			name: "WithNotFound",
+			args: args{haystack: "aaaaa", needle: "baa"},
+			want: -1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := strStr(tt.args.haystack, tt.args.needle); got != tt.want {
+				t.Errorf("strStr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
