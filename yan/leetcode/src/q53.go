4ec724b2eef1671fb09e572d08dbb7173e5d1827
Loki <lokiworks.yen@gmail.com>
2022-07-18 22:51:01 +0800

    feat: add answer for the question fifty-three.

---
+++ yan/leetcode/src/q53.go
@@ -0,0 +1,18 @@
+package main
+
+// 参考官方题解
+// 用到的知识点：动态规划
+func maxSubArray(nums []int) int {
+	max := nums[0]
+	for i := 1; i < len(nums); i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+
+	return max
+
+}
