d898532f6e67e0cc4aa4331404957098b9584903
peter <loki.yen@outlook.com>
2022-07-19 09:57:40 +0800

    feat: add answer for the question sixty-seven.

---
+++ yan/leetcode/src/q67.go
@@ -0,0 +1,44 @@
+package main
+
+import "strconv"
+
+func addBinary(a string, b string) string {
+	aLen := len(a)
+	bLen := len(b)
+	remainder := 0
+	result := ""
+	i, j := aLen-1, bLen-1
+	for ; i >= 0 && j >= 0; i, j = i-1, j-1 {
+		av, bv := int(a[i]-'0'), int(b[j]-'0')
+		temp := (av + bv + remainder) % 2
+		remainder = (av + bv + remainder) / 2
+		result = result + strconv.Itoa(temp)
+	}
+	for ; i >= 0; i = i - 1 {
+		temp := (int(a[i]-'0') + remainder) % 2
+		remainder = (int(a[i]-'0') + remainder) / 2
+		result = result + strconv.Itoa(temp)
+	}
+	for ; j >= 0; j = j - 1 {
+		temp := (int(b[j]-'0') + remainder) % 2
+		remainder = (int(b[j]-'0') + remainder) / 2
+		result = result + strconv.Itoa(temp)
+	}
+
+	if remainder != 0 {
+		result = result + strconv.Itoa(remainder)
+	}
+
+	reverseResult := ""
+
+	for _, v := range result {
+		reverseResult = string(v) + reverseResult
+	}
+	return reverseResult
+
+}
+
+func main() {
+	r := addBinary("11", "1")
+	println(r)
+}
---
+++ yan/leetcode/src/q67_test.go
@@ -0,0 +1,34 @@
+package main
+
+import "testing"
+
+func Test_addBinary(t *testing.T) {
+	type args struct {
+		a string
+		b string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "With11Add1",
+			args: args{a: "11", b: "1"},
+			want: "100",
+		},
+		{
+			name: "With1010Add1011",
+			args: args{a: "1010", b: "1011"},
+			want: "10101",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addBinary(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("addBinary() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
