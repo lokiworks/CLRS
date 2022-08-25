443d25aea69aff7692f5e9f875bf6b04090fd9e9
peter <loki.yen@outlook.com>
2022-08-25 10:00:49 +0800

    feat: add implement of the rbDelete

--- exercise/src/chapter13/red_black_tree.go
+++ exercise/src/chapter13/red_black_tree.go
@@ -131,5 +131,103 @@ func rbTransplant(T *RedBlackTree, u, v *RedBlackNode) {
 	v.p = u.p
 }
 
+func treeMinimum(n *RedBlackNode) *RedBlackNode {
+	if n == nil {
+		return nil
+	}
+	for n.left != nil {
+		n = n.left
+	}
+	return n
+}
+
+func rbDelete(T *RedBlackTree, z *RedBlackNode) {
+	y := z
+	yOriginalColor := y.color
+	var x *RedBlackNode
+	if z.left == nil {
+		x = z.right
+		rbTransplant(T, z, z.right) // replace z by its right child
+	} else if z.right == nil {
+		x = z.left
+		rbTransplant(T, z, z.left)
+	} else {
+		y := treeMinimum(z.right) // y is z's successor
+		yOriginalColor = y.color
+		x = y.right
+		if y != z.right {
+			rbTransplant(T, y, y.right)
+			y.right = z.right
+			y.right.p = y
+		} else {
+			x.p = y
+		}
+		rbTransplant(T, z, y)
+		y.left = z.left
+		y.left.p = y
+		y.color = z.color
+	}
+	// if any red-black violations occurred, correct them
+	if yOriginalColor == BLACK {
+		rbDeleteFixup(T, x)
+	}
+
+}
+
+func rbDeleteFixup(T *RedBlackTree, x *RedBlackNode) {
+	for x != T.root && x.color == BLACK {
+		if x == x.p.left { // is x a left child
+			w := x.p.right
+			if w.color == RED {
+				w.color = BLACK
+				x.p.color = RED
+				leftRotation(T, x.p)
+				w = x.p.right
+			}
+			if w.left.color == BLACK && w.right.color == BLACK {
+				w.color = RED
+				x = x.p
+			} else {
+				if w.right.color == BLACK {
+					w.left.color = BLACK
+					w.color = RED
+					rightRotation(T, w)
+					w = x.p.right
+				}
+				w.color = x.p.color
+				x.p.color = BLACK
+				w.right.color = BLACK
+				leftRotation(T, x.p)
+				x = T.root
+			}
+		} else {
+			w := x.p.left
+			if w.color == RED {
+				w.color = BLACK
+				x.p.color = RED
+				rightRotation(T, x.p)
+				w = x.p.left
+			}
+			if w.right.color == BLACK && w.left.color == BLACK {
+				w.color = RED
+				x = x.p
+			} else {
+				if w.left.color == BLACK {
+					w.right.color = BLACK
+					w.color = RED
+					leftRotation(T, w)
+					w = x.p.left
+				}
+				w.color = x.p.color
+				x.p.color = BLACK
+				w.left.color = BLACK
+				rightRotation(T, x.p)
+				x = T.root
+			}
+		}
+	}
+	x.color = BLACK
+}
+
 func main() {
 }
